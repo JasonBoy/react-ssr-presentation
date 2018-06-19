d50c22055cbbd6de13510db482f1304262061e13
Jason <jasonlikenfs@gmail.com>
2018-06-19 16:29:37 +0800

    add fullpage

--- src/App.js
+++ src/App.js
@@ -1,20 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Fullpage, HorizontalSlider, Slide} from 'fullpage-react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+  }
+
+  state = {
+    fullPageOptions: null,
+  };
+
+  componentDidMount() {
+    const fullPageOptions = {
+      // for mouse/wheel events
+      // represents the level of force required to generate a slide change on non-mobile, 10 is default
+      scrollSensitivity: 7,
+
+      // for touchStart/touchEnd/mobile scrolling
+      // represents the level of force required to generate a slide change on mobile, 10 is default
+      touchSensitivity: 7,
+      scrollSpeed: 300,
+      hideScrollBars: true,
+      enableArrowKeys: true
+    };
+
+    const horizontalSliderProps = {
+      name: 'horizontalSlider1', // name is required
+      infinite: true, // enable infinite scrolling
+    };
+
+    horizontalSliderProps.slides = [
+      <Slide> Slide 2.1 </Slide>,
+      <Slide> Slide 2.2 </Slide>
+    ];
+
+    const slides = [
+      <Slide> Slide 1 </Slide>,
+      <HorizontalSlider {...horizontalSliderProps}></HorizontalSlider>,
+      <Slide> Slide 3 </Slide>
+    ];
+    fullPageOptions.slides = slides;
+
+    this.setState({fullPageOptions});
+  }
+
+
+
+
   render() {
+    if(!this.state.fullPageOptions) return null;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Fullpage {...this.state.fullPageOptions}></Fullpage>
+    )
+    ;
   }
 }
 
